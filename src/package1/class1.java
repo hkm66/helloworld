b7e81bee9e1d4f579924925051749ff04c75788b
bluehkm <bluehakim@protonmail.com>
2022-12-08 21:32:23 +0300

    created class2

---
+++ src/package1/class1.java
@@ -0,0 +1,10 @@
+package package1;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class class1 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        // hello1
+    }
+}
